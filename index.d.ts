a7504bc3a5de8d6aaa95a4f9feaa78af116b8893
gotjosh <gitlab@begreen.nu>
2020-06-21 18:18:46 +0100

    NoI - formatting

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 import { RxDocument } from 'rxdb'
 declare module 'rxdb-history' {
-  export const DocumentHistory: RxDB = {  }
+  export const DocumentHistory: RxDB = {}
   export type getHistoryMap = () => Promise<Map<number, RxDocument>>
 }
